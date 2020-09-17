0ca6c3bf355764a9f7803754e1825440b056ac8a
Niko Kynsijarvi <niko.kynsijarvi@hotmail.com>
2020-09-17 14:20:31 +0300

    tehtävät 2.5 asti

--- osa2/part1/src/components/Course.js
+++ osa2/part1/src/components/Course.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
-const Course = ({ course }) => {
-    return (
-        <p>{course.content}</p>
-    )
+const Course = ({ courses }) => {
+	var contents = courses.map(function (content) {
+		return (
+			<div>
+				<h1>{content.name}</h1>
 
+				{content.parts.map((part) => (
+					<p>
+						{part.name} {part.exercises}
+					</p>
+				))}
+				<p>
+					<strong>
+						{" "}
+						total of{" "}
+						{content.parts.reduce(function (sum, number) {
+							return sum + number.exercises;
+						}, 0)}{" "}
+						exercises
+					</strong>
+				</p>
+			</div>
+		);
+	});
 
-}
+	return <div>{contents}</div>;
+};
+
+export default Course;
