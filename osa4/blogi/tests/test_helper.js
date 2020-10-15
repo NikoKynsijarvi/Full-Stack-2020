e3c85bb4f00f2b2939bc449a387272af25fe91ed
Niko Kynsijarvi <niko.kynsijarvi@hotmail.com>
2020-10-15 20:12:45 +0300

    4.15

--- osa4/blogi/tests/test_helper.js
+++ osa4/blogi/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require("../models/blogs");
+const User = require("../models/user");
 
 const initialBlogs = [
 	{
@@ -24,7 +25,12 @@ const blogsInDb = async () => {
 	const blogs = await Blog.find({});
 	return blogs.map((blog) => blog.toJSON());
 };
+const usersInDb = async () => {
+	const users = await User.find({});
+	return users.map((u) => u.toJSON());
+};
 module.exports = {
 	initialBlogs,
 	blogsInDb,
+	usersInDb,
 };
